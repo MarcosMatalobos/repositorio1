7f43ea14091bbf682c2e17855a0e4a288623c89e
marcos matalobos <markitosmatalobos@gmail.com>
2022-09-12 20:53:57 -0300

    ejercicios pdf01

---
+++ Ejercicio1pdf01.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     12/09/2022
+# Copyright:   (c) Administrator 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    variable1 = 5
+    variable2 = 10
+    suma=variable1 + variable2
+    print ("Resultado:",suma)
+if __name__ == '__main__':
+    main()
---
+++ Ejercicio2pdf01.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     12/09/2022
+# Copyright:   (c) Administrator 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    Nombre = input ("Ingrese su nombre")
+    Apellido = input ("Ingrese apellido")
+
+    print( " Nombre de usuario: ", str (Nombre), str(Apellido))
+
+if __name__ == '__main__':
+    main()
