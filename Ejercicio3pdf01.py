7a3960512c8e6c24b3c3ff8ffebb0a1704c51e5c
marcos matalobos <markitosmatalobos@gmail.com>
2022-09-13 17:17:39 -0300

    Ejercicio3 v1

---
+++ Ejercicio3pdf01.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     12/09/2022
+# Copyright:   (c) Administrator 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+   Nombre1 = input ("Ingresar nombre completo: ")
+   Apellido = input ("Ingresar apellido: ")
+   DNI = input ("Ingresar DNI: ")
+
+   Nombre = (str (Nombre1 + Apellido), (DNI))
+
+   print("El socio es:", Nombre)
+
+
+if __name__ == '__main__':
+    main()
+#EJERCICIO 3 PDF1
